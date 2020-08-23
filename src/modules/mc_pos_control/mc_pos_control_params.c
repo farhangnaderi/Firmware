315939ed68b624d2d34d1fcb089f91c9dffb7c18
FARHANG <46557204+farhangnaderi@users.noreply.github.com>
2020-08-23 19:42:44 +0300

    Update mc_pos_control_params.c

--- src/modules/mc_pos_control/mc_pos_control_params.c
+++ src/modules/mc_pos_control/mc_pos_control_params.c
@@ -109,7 +109,7 @@ PARAM_DEFINE_INT32(MPC_THR_CURVE, 0);
  */
 PARAM_DEFINE_FLOAT(GND_THR_MIN, 0.0f);
 
-/** Added this to make the idle throttle changeable
+/** Added this to make the idle throttle changed
 */
 
 PARAM_DEFINE_FLOAT(MPC_THR_MAX, 1.0f);
